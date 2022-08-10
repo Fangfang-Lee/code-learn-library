a9f68032892900ec71713cbf82476da4551bf4e4
Fangfang-Lee <justase@163.com>
2022-08-10 10:38:45 +0800

    first commit

---
+++ java-coretech-1/src/chapter5/ObjectAnalyzerTest.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectAnalyzerTest {
+
+  public static void main(String[] args) throws IllegalAccessException {
+    List<Integer> squares = new ArrayList<>();
+    for (int i = 1; i <= 5; i++) {
+      squares.add(i * i);
+    }
+    System.out.println(new ObjectAnalyzer().toString(squares));
+  }
+}
