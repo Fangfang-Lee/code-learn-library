73b8d0ca3d8f913ebb3225c92016e5bf3ce2dfaf
Fangfang-Lee <justase@163.com>
2022-10-24 00:09:54 +0800

    thread

---
+++ java-coretech-1/src/main/java/com/jason/chapter12/threads/Bank.java
@@ -0,0 +1,15 @@
+package com.jason.chapter12.threads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Bank {
+  private int id;
+  private int salary;
+
+  public void transfer(int i, int i1, double amount) {
+    
+  }
+}
