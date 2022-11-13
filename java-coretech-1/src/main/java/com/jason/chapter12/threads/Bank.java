cbdaa0ad57810f92847570400d763d24daf324be
Fangfang-Lee <justase@163.com>
2022-11-13 23:39:27 +0800

    thread

--- java-coretech-1/src/main/java/com/jason/chapter12/threads/Bank.java
+++ java-coretech-1/src/main/java/com/jason/chapter12/threads/Bank.java
@@ -3,13 +3,38 @@ package com.jason.chapter12.threads;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Arrays;
+
 @Data
 @AllArgsConstructor
 public class Bank {
-  private int id;
-  private int salary;
+  private final double[] accounts;
 
-  public void transfer(int i, int i1, double amount) {
-    
+  public Bank(int n, double initialBalance) {
+    accounts = new double[n];
+    Arrays.fill(accounts, initialBalance);
+  }
+
+  public void transfer(int from, int to, double amount) {
+    if (accounts[from] < amount) {
+      return;
+    }
+    System.out.print(Thread.currentThread());
+    accounts[from] -= amount;
+    System.out.printf(" %10.2f from %d to %d", amount, from, to);
+    accounts[to] += amount;
+    System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+  }
+
+  private double getTotalBalance() {
+    double sum = 0;
+    for (double a : accounts) {
+      sum += a;
+    }
+    return sum;
+  }
+
+  public int size() {
+    return accounts.length;
   }
 }
---
+++ java-coretech-1/src/main/java/com/jason/chapter12/unsynch/UnsynchBankTest.java
@@ -0,0 +1,30 @@
+package com.jason.chapter12.unsynch;
+
+import com.jason.chapter12.threads.Bank;
+
+public class UnsynchBankTest {
+
+  public static final int NACCOUNTS = 100;
+  public static final double INITIAL_BALANCE = 1000;
+  public static final double MAX_AMOUNT = 1000;
+  public static final int DELAY = 10;
+
+  public static void main(String[] args) {
+    Bank bank = new Bank(NACCOUNTS, INITIAL_BALANCE);
+    for (int i = 0; i < NACCOUNTS; i++) {
+      int fromAccount = i;
+      Runnable r = () -> {
+        try {
+          while (true) {
+            int toAccount = (int) (bank.size() * Math.random());
+            double amount = MAX_AMOUNT * Math.random();
+            bank.transfer(fromAccount, toAccount, amount);
+            Thread.sleep((int) (DELAY * Math.random()));
+          }
+        } catch (InterruptedException e) {}
+      };
+      Thread t = new Thread(r);
+      t.start();
+    }
+  }
+}
